94d1ee17bfb27ecd90bfcd2c2aa8e6594b845c79
Carlos Hinojosa <carlos.hinojosa93@gmail.com>
2022-09-17 11:32:44 -0500

    adding resources of curses Sept 16 and 17

--- ion_grid_ion_item/src/app/tab1/tab1.page.ts
+++ ion_grid_ion_item/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,26 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  nombre: string = "";
+  
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      message: 'El nombre debe tener menos de 10 caracteres',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  guardarLen() {
+    if ((this.nombre.length) > 10) {
+      this.presentAlert()
+      this.nombre = "";
+    }
+
+  }
 
 }
--- ion_grid_ion_item/src/app/tab3/tab3.page.ts
+++ ion_grid_ion_item/src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,30 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Ejemplo Alerta',
+      message: "Esta es una alerta",
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log("se dio click en cancelar");
+          },
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            console.log("se dio click en confirmar");
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }

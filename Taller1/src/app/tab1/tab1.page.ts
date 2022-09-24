a8ea9be9399ee167e2aded721a21c28c3cc32279
Carlos Hinojosa <carlos.hinojosa93@gmail.com>
2022-09-24 11:39:55 -0500

    update taller1 tab2

--- Taller1/src/app/tab1/tab1.page.ts
+++ Taller1/src/app/tab1/tab1.page.ts
@@ -10,33 +10,102 @@ import { ShareDataService } from '../services/share-data.service';
 export class Tab1Page {
 
   nombre: string = "";
-  
-  constructor(private alertController: AlertController, private shareService: ShareDataService) {}
+  apellido: string ="";
+  email: string ="";
+  ciudad: string="";
+  edad: number;
+  telefono: number;
+  equipo: string="";
+  password: string="";
 
-  async presentAlert() {
+  registros=[];
+
+  constructor(private alertController: AlertController, private shareDataService: ShareDataService) {}
+
+  async presentAlert(mensaje, titulo="Advertencia", botones=['OK'] ) {
     const alert = await this.alertController.create({
-      header: 'Alerta',
-      message: 'El nombre debe tener menos de 15 caracteres',
-      buttons: ['OK'],
+      header: titulo,
+      message: mensaje,
+      buttons: botones,
     });
-
+    
     await alert.present();
   }
 
-  guardarLen() {
-    if ((this.nombre.length) > 10) {
-      this.presentAlert()
+  checkLen(id, num=15){
+    if (id==1 && this.nombre.length > num) {
+      this.presentAlert('El nombre debe tener menos de ' + num +' caracteres')
       this.nombre = "";
     }
 
+    if (id==2 && this.apellido.length > num) {
+      this.presentAlert('El apellido debe tener menos de ' + num +' caracteres')
+      this.apellido = "";
+    }
+
+    if (id==3 && this.ciudad.length > num) {
+      this.presentAlert('La ciudad debe tener menos de ' + num +' caracteres')
+      this.ciudad = "";
+    }
+
+    if (id==4 && this.password.length > num) {
+      this.presentAlert('La contraseña debe tener menos de ' + num +' caracteres')
+      this.password = "";
+    }
   }
 
-  saveJson() {
-    let jsonObj = {
-      nombre: this.nombre
+  checkEmail(){
+    if (!this.email.includes("@")){
+      this.presentAlert("El email debe contener el @");
+      this.email = "";
     }
-    let data = JSON.stringify(jsonObj);
-    this.shareService.saveData(data)
+  }
+
+  checkNumber(id){
+    if (id==1 && isNaN(Number(this.edad))) {
+      this.presentAlert("Debe ingresar un número de edad valido");
+      this.edad = 0;
+    }
+
+    if (id==2 && isNaN(Number(this.telefono))) {
+      this.presentAlert("Debe ingresar un número de telefono valido");
+      this.telefono = 0;
+    }
+  }
+
+  resetValues() {
+    this.nombre= "";
+    this.apellido="";
+    this.email="";
+    this.ciudad="";
+    this.edad=undefined;
+    this.telefono=undefined;
+    this.equipo="";
+    this.password="";
+  }
+
+  pushCancel() {
+    this.presentAlert("Usted ha cancelado esta operación");
+    this.resetValues();
+  }
+
+  registerform(form) {
+    let jsonObj = {
+      nombre: this.nombre,
+      apellido: this.apellido,
+      email: this.email,
+      ciudad: this.ciudad,
+      edad: this.edad,
+      telefono: this.telefono,
+      equipo: this.equipo,
+      password: this.password
+    }
+
+    this.registros.push(jsonObj) // [obj1, obj2....]
+
+    this.shareDataService.saveData(this.registros)
+
+    this.resetValues()
   }
 
 }
--- Taller1/src/app/tab2/tab2.page.ts
+++ Taller1/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
+import { ShareDataService } from '../services/share-data.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  data: any;
+  search: string = "";
+  filteredData: any;
+  constructor(private shareService: ShareDataService) {}
 
+  updateFilterData() {
+    if (this.search.length > 0) {
+      this.filteredData = this.data.filter((json) => json['apellido'].startsWith(this.search));
+    }
+  }
+  
+  updateData() {
+    this.data = this.shareService.readData();
+    if (!this.data){
+      console.log("No existe ningun registro");
+      this.data = []
+    }
+  }
 }

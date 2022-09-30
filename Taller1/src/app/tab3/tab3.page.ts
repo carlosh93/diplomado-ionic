954f9460daa70b96fc5806d16e8be199ac3e2926
Carlos Hinojosa <carlos.hinojosa93@gmail.com>
2022-09-30 18:39:56 -0500

    remove tab3

--- Taller1/src/app/tab3/tab3.page.ts
+++ Taller1/src/app/tab3/tab3.page.ts
@@ -10,31 +10,7 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab3Page {
 
-  data: any;
-  num: number= 1;
+ 
   constructor(private shareService: ShareDataService, private alertController: AlertController) {}
-
-  ionViewWillEnter(){
-    this.updateData()
-    console.log(JSON.stringify(this.data))
-  }
-
-  updateData() {
-    this.data = this.shareService.readData();
-    if (!this.data){
-      console.log("No existe ningun registro");
-      this.data = []
-    }
-  }
-
-  async presentAlert(mensaje, titulo="Advertencia", botones=['OK'] ) {
-    const alert = await this.alertController.create({
-      header: titulo,
-      message: mensaje,
-      buttons: botones,
-    });
-    
-    await alert.present();
-  }
 
 }

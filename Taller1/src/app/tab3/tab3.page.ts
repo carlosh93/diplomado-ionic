f13e8814810fcc031a3b6753f89e611361b00b1d
Carlos Hinojosa <carlos.hinojosa93@gmail.com>
2022-10-08 11:07:12 -0500

    update Taller 1 and adding photogalery example

--- Taller1/src/app/tab3/tab3.page.ts
+++ Taller1/src/app/tab3/tab3.page.ts
@@ -13,4 +13,27 @@ export class Tab3Page {
   data: any;
   constructor(private shareService: ShareDataService, private alertController: AlertController) {}
 
+  ionViewWillEnter(){
+    this.updateData()
+    console.log(JSON.stringify(this.data))
+  }
+
+  updateData() {
+    this.data = this.shareService.readData();
+    if (!this.data){
+      console.log("No existe ningun registro");
+      this.data = []
+    }
+  }
+
+  async presentAlert(mensaje, titulo="Advertencia", botones=['OK'] ) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensaje,
+      buttons: botones,
+    });
+    
+    await alert.present();
+  }
+
 }
